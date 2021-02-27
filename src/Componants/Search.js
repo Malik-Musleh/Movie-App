978e084efa132919ee6a1b6b66329e8f726b7caf
malek88888 <jmzomalek@gmail.com>
2021-02-27 23:22:09 +0200

    Revert "redux base files created"
    This reverts commit fdbff23d176a78512a6aaa7d2483e7d9bdd02a72.

--- src/Componants/Search.js
+++ src/Componants/Search.js
@@ -1,20 +1,11 @@
 import React  from 'react';
-import { useSelector } from 'react-redux';
 
-
-function Search({state,handleInput,searchMovie}) {
-  console.log("state",state);
-  // console.log("handleInput",handleInput);
-  // console.log("searchMovie",searchMovie);
-  console.log(useSelector(s=>s)); 
+function Search({handleInput,searchMovie}) {
   return (
 <section className="search-box-wrap">
-    <input className="search-box" placeholder="Search for a movie ... " 
-    // onKeyDown={handleInput}
-    onChange={(e) => handleInput(e.target.value)}
-    onKeyPress={(e)=>searchMovie(e.target.value)}
-    />
-    <button onClick={handleInput} >test</button>
+    <input className="search-box" placeholder="Search for a movie ... " name=""
+    onChange={handleInput}
+    onKeyPress={searchMovie}/>
 </section>
   );
 }
