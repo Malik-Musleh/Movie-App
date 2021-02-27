7a7e2f69a8be982d8da8bb62a5a6d8a3d7f15fc1
malek88888 <jmzomalek@gmail.com>
2021-02-27 07:44:25 +0200

    react app created

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+;
+import './App.css';
+
+function App() {
+
+
+
+
+  
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Movie App</h1>
+      </header>
+      <main>
+      </main>
+    </div>
+  );
+}
+
+export default App;
