fdbff23d176a78512a6aaa7d2483e7d9bdd02a72
malek88888 <jmzomalek@gmail.com>
2021-02-27 23:05:23 +0200

    redux base files created

--- src/App.js
+++ src/App.js
@@ -1,61 +1,76 @@
 import React, { useState } from 'react';
-import Search from './Componants/Search';
-import Results from './Componants/Results';
-import Popup from './Componants/Popup';
-import axios from 'axios';
+import { Provider } from 'react-redux';
+import {  ContainerSearch, ContainerResults, ContainerPopup } from './counter/container';
+import { store } from './counter/configure-store';
+import { useSelector } from 'react-redux';
+
+
+// import Search from './Componants/Search';
+// import Results from './Componants/Results';
+// import Popup from './Componants/Popup';
+// import axios from 'axios';
 import './App.css';
 
 function App() {
-  const OMDbAPI = "http://www.omdbapi.com/?i=tt3896198&apikey=df39af2c"
-  const [movie, setSearch] = useState({
-    s: "",
-    results: [],
-    selected: {},
-    overveiw:[]
-  });
-
-  const handleInput = (e) => {
-    setSearch(pre => { return { ...pre, s: e.target.value } });
-  }
-
-  const searchMovie = (e) => {
-    if (e.key === "Enter") {
-      axios(OMDbAPI + "&s=" + movie.s).then(({ data }) => {
-        let results = data.Search;
-        console.log(data);
-        setSearch(prevState => {
-          return { ...prevState, results: results }
-        })
-      });
-    }
-  }
-  
-  const openPopup = title => {
-    axios(OMDbAPI + "&t=" + title).then(({ data }) => {
-      setSearch(prevState => {
-        return { ...prevState, selected: data }
-      })
-    });
-  }
-
-  const closePopup = () => {
-    setSearch(prevState => {
-      return { ...prevState, selected: {} }
-    })
-  }
-
+  // const movie =useSelector(state=>state.selected);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie App</h1>
       </header>
-      <main>
-        <Search handleInput={handleInput} searchMovie={searchMovie} />
-        <Results results={movie.results} openPopup={openPopup} />
-        {typeof movie.selected.Title != "undefined" ? <Popup selected={movie.selected} closePopUp={closePopup} /> : false}
-      </main>
+      <Provider store={store}>
+        <main>
+        <ContainerSearch/>
+        {/* <ContainerResults/> */}
+        {/* <ContainerPopup/> */}
+        {/* {typeof movie.selected.Title != "undefined" ? <ContainerPopup/> : false} */}
+        </main>
+      </Provider>
+
+
     </div>
   );
 }
 
 export default App;
+
+{/* <Search handleInput={handleInput} searchMovie={searchMovie} />
+<Results results={movie.results} openPopup={openPopup} />
+{typeof movie.selected.Title != "undefined" ? <Popup selected={movie.selected} closePopUp={closePopup} /> : false} */}
+// const OMDbAPI = "http://www.omdbapi.com/?i=tt3896198&apikey=df39af2c"
+// const [movie, setSearch] = useState({
+//   s: "",
+//   results: [],
+//   selected: {},
+//   overveiw:[]
+// });
+
+// const handleInput = (e) => {
+//   setSearch(pre => { return { ...pre, s: e.target.value } });
+// }
+
+// const searchMovie = (e) => {
+//   if (e.key === "Enter") {
+//     axios(OMDbAPI + "&s=" + movie.s).then(({ data }) => {
+//       let results = data.Search;
+//       console.log(data);
+//       setSearch(prevState => {
+//         return { ...prevState, results: results }
+//       })
+//     });
+//   }
+// }
+
+// const openPopup = title => {
+//   axios(OMDbAPI + "&t=" + title).then(({ data }) => {
+//     setSearch(prevState => {
+//       return { ...prevState, selected: data }
+//     })
+//   });
+// }
+
+// const closePopup = () => {
+//   setSearch(prevState => {
+//     return { ...prevState, selected: {} }
+//   })
+// }
--- src/Componants/Search.js
+++ src/Componants/Search.js
@@ -1,11 +1,20 @@
 import React  from 'react';
+import { useSelector } from 'react-redux';
 
-function Search({handleInput,searchMovie}) {
+
+function Search({state,handleInput,searchMovie}) {
+  console.log("state",state);
+  // console.log("handleInput",handleInput);
+  // console.log("searchMovie",searchMovie);
+  console.log(useSelector(s=>s)); 
   return (
 <section className="search-box-wrap">
-    <input className="search-box" placeholder="Search for a movie ... " name=""
-    onChange={handleInput}
-    onKeyPress={searchMovie}/>
+    <input className="search-box" placeholder="Search for a movie ... " 
+    // onKeyDown={handleInput}
+    onChange={(e) => handleInput(e.target.value)}
+    onKeyPress={(e)=>searchMovie(e.target.value)}
+    />
+    <button onClick={handleInput} >test</button>
 </section>
   );
 }
