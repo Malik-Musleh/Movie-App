cf537e5df2ba890be0138f3026232b0c81c2012c
malek88888 <jmzomalek@gmail.com>
2021-02-28 14:07:19 +0200

    movies titles stored into the Local Storge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Search from './Componants/Search';
 import Results from './Componants/Results';
 import Home from './Componants/Home';
@@ -18,7 +25,10 @@ function App() {
     page: "1",
     wishList: []
   });
-
+  localStorage["array"] = ["s", 2, { 3: 3, 4: 4, 5: 4 }];
+  // localStorage.array[1]="1"
+  // localStorage["names"] = new Array();
+  // localStorage.names[0] = prompt("New member name?");
 
   // movie.s.length == 0 ? setSearch((pre => { return { ...pre, results: [] } })) : setSearch((pre => { return { ...pre, All: [] } }))
   const handleInput = (e) => {
@@ -36,10 +46,11 @@ function App() {
       });
     }
   }
+
   const addToWish = (e) => {
-    const a=movie.wishList
+    const a = movie.wishList
     a.push(e)
-    setSearch(pre => { return { ...pre, wishList:a} })
+    setSearch(pre => { return { ...pre, wishList: a } })
   }
   // const getAllMovies = () => {
   //   axios(OMDbAPI + "&s=Batman&page=" + movie.page).then(({ data }) => {
@@ -54,6 +65,7 @@ function App() {
     axios(OMDbAPI + "&t=" + title).then(({ data }) => {
       setSearch(prevState => {
         return { ...prevState, selected: data }
+
       })
     });
   }
@@ -63,21 +75,34 @@ function App() {
       return { ...prevState, selected: {} }
     })
   }
-  // useEffect(() => getAllMovies(),[])
+  const set = () => movie.wishList.length == 0 ? true : movie.wishList.forEach(element => {
+    let m = localStorage.getItem('myData');
+    var s;
+    m.length<1? s=m.concat( element.Title): s=m.concat(","+ element.Title)
+    localStorage.setItem('myData',s);
+  });
+  useEffect(() => set)
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Movie App</h1>
-      </header>
-      <main>
-        <Search handleInput={handleInput} searchMovie={searchMovie} />
-        <Results results={movie.results} openPopup={openPopup} />
+    <Router>
+      <div className="App">
+        <Route exact path="/">
+          <header className="App-header">
+            <h1>Movie App</h1>
+          </header>
+          <main>
+            <Search handleInput={handleInput} searchMovie={searchMovie} />
+            <Results results={movie.results} openPopup={openPopup} />
+          </main>
+        </Route>
+        <Route exact path="/wishlist">
+          {/* <Wishlist results={movie.wishList} openPopup={openPopup} /> */}
+        </Route>
         {/* <Home results={movie.All} ope nPopup={openPopup}  /> */}
         {/* <button onClick={() => setSearch(prevState =>{return {...prevState, page: (Number(movie.page++)).toString()}})}>+</button> */}
         {/* {movie.page===1? (true):<button onClick={() => setSearch(prevState =>{return {...prevState, page: (Number(movie.page--)).toString()}})}>-</button>} */}
         {typeof movie.selected.Title != "undefined" ? <Popup selected={movie.selected} addToWish={addToWish} closePopUp={closePopup} /> : false}
-      </main>
-    </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Componants/Wishlist.js
+++ src/Componants/Wishlist.js
@@ -2,14 +2,21 @@ import React, { useState } from 'react';
 import Result from './Result';
 
 function WishList({ results, openPopup }) {
-    const wishList = results.map(result => (<div key={result.imdbID} className="result" onClick={() => openPopup(result.Title)}>
-        <img src={result.Poster} ></img>
-        <h3>{result.Title}</h3>
-    </div>
-    ))
+    const wish =localStorage.getItem('myData');
+    // if (wish) {
+    //     wish.map(result => (<div key={result.imdbID} className="result" onClick={() => openPopup(result.Title)}>
+    //        <img src={result.Poster} ></img>
+    //        <h3>{result.Title}</h3>
+    //    </div>
+    //    ))
+    // }
     return (
         <section className="results">
-            {wishList}
+            {wish? wish.map(result => (<div key={result.imdbID} className="result" onClick={() => openPopup(result.Title)}>
+           <img src={result.Poster} ></img>
+           <h3>{result.Title}</h3>
+       </div>
+       )):false}
         </section>
     );
 }
